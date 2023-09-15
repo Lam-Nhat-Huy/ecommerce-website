d7601a5dedf7ffdf90f12680293714a1629c17e9
huydev <lamnhathuy0393418721@gmail.com>
2023-09-15 14:07:36 +0700

    update function registerAdmin

--- admin/core/code.php
+++ admin/core/code.php
@@ -3,44 +3,12 @@ require_once './config/database.php';
 require_once './admin/core/function.php';
 
 if (isset($_POST['signup-admin'])) {
-    $error = "";
     $username = mysqli_real_escape_string($conn, $_POST['username']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $password = mysqli_real_escape_string($conn, $_POST['password']);
     $role_id = mysqli_real_escape_string($conn, $_POST['role_id']);
     $cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']);
-    // Hàm dùng để redex name
-    if (empty($username) || !preg_match('/^[a-zA-Z ]+$/', $username)) {
-        $error .= "Please enter a valid name <br>";
-    }
-    // Hàm dùng để redex email
-    if (empty($email) || !preg_match('/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+\.[A-Za-z]{2,6}$/', $email)) {
-        $error .= "Please enter a valid email address <br>";
-    }
-    // Hàm kiểm tra nhập lại mật khẩu
-    if ($password !== $cpassword) {
-        $error .= "Password do not match <br>";
-    }
-    // Mật khẩu ít nhất phải trên 6 kí tự
-    if (strlen($password) < 4) {
-        $error .= "Password must be atleast 4 characters long <br>";
-    }
-    // Kiểm tra trùng lặp email
-    $checkEmailQuery = "SELECT * FROM users WHERE email = '$email'";
-    $checkEmailResult = mysqli_query($conn, $checkEmailQuery);
-    if (mysqli_num_rows($checkEmailResult) > 0) {
-        $error .= "Email already exists <br>";
-    }
-    // Run
-    if ($error == "") {
-        $sql = "INSERT INTO users (username, email, password, role_id) VALUES ('$username', '$email', '$password', '$role_id')";
-        $query = mysqli_query($conn, $sql);
-        header("Location: ./index.php?pages=login");
-    } else {
-        header("Location: ./index.php?pages=register");
-    }
-    // Lưu chữ thông tin lỗi
-    $_SESSION['error'] = $error;
+    $query = registerAdmin($username, $email, $password, $role_id, $cpassword);
 }
 
 if (isset($_POST['login-admin'])) {
--- admin/core/function.php
+++ admin/core/function.php
@@ -7,6 +7,7 @@ function show($data)
     print_r($data);
     echo "</pre>";
 }
+
 // hàm để hiển thị thông báo
 function check_error()
 {
@@ -14,4 +15,45 @@ function check_error()
         echo $_SESSION['error'];
         unset($_SESSION['error']);
     }
+}
+
+
+function registerAdmin($username, $email, $password, $role_id, $cpassword)
+{
+    global $conn;
+    $error = "";
+
+    // Kiểm tra trùng lặp email
+    $checkEmailQuery = "SELECT * FROM users WHERE email = '$email'";
+    $checkEmailResult = $conn->query($checkEmailQuery);
+    if ($checkEmailResult->num_rows > 0) {
+        $error .= "Email already exists <br>";
+    }
+    // Hàm dùng để redex name
+    if (empty($username) || !preg_match('/^[a-zA-Z ]+$/', $username)) {
+        $error .= "Please enter a valid name <br>";
+    }
+    // Hàm dùng để redex email
+    if (empty($email) || !preg_match('/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+\.[A-Za-z]{2,6}$/', $email)) {
+        $error .= "Please enter a valid email address <br>";
+    }
+    // Hàm kiểm tra nhập lại mật khẩu
+    if ($password !== $cpassword) {
+        $error .= "Password do not match <br>";
+    }
+    // Mật khẩu ít nhất phải trên 6 kí tự
+    if (strlen($password) < 4) {
+        $error .= "Password must be atleast 4 characters long <br>";
+    }
+
+    if ($error == "") {
+        $checkUserQuery = "INSERT INTO users (username, email, password, role_id) VALUES ('$username', '$email', '$password', '$role_id')";
+        $checkUserQuery = mysqli_query($conn, $checkUserQuery);
+        header("Location: ./index.php?pages=login");
+    } else {
+        header("Location: ./index.php?pages=register");
+    }
+
+    // Lưu chữ thông tin lỗi
+    $_SESSION['error'] = $error;
 }
