329de79e0716d0a4b80a9e9ccb4af873292b3fcb
huydev <lamnhathuy0393418721@gmail.com>
2023-09-18 00:36:57 +0700

    thêm chức năng hiển thị sản phẩm

--- admin/resources/product/ProductList.php
+++ admin/resources/product/ProductList.php
@@ -26,7 +26,40 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
+                    $query_course = mysqli_query($conn, "SELECT c.id, c.name, c.image, c.image, c.price, c.description, ct.category_name
+                        FROM products c, category ct WHERE c.category_id = ct.id");
+                    if (mysqli_num_rows($query_course) >  0) {
+                        while ($fetch_course = mysqli_fetch_assoc($query_course)) {
+                    ?>
+                            <tr style="vertical-align: middle;">
+                                <td><?= $fetch_course['id'] ?></td>
+                                <td><?= $fetch_course['name'] ?></td>
+                                <td>
+                                    <img src="./admin/uploads/<?= $fetch_course['image'] ?>" alt="" width="100px">
+                                </td>
+                                <td><?= $fetch_course['price'] ?></td>
+                                <td class="td-width"><?= $fetch_course['description'] ?></td>
+                                <td><?= $fetch_course['category_name'] ?></td>
 
+                                <td class="d-flex justify-content-evenly ">
+                                    <a href="./index.php?pages=product&action=edit&id=<?= $fetch_course['id'] ?>" class="btn btn-success btn-sm"><i class="fas fa-pencil-alt"></i>
+                                    </a>
+
+                                    <form action="./admin/core/CodeAdminLogin.php" method="post">
+                                        <button onclick="return confirm('Bạn có chắc chắn muốn xóa? ')" type="submit" class="btn btn-danger btn-sm" name="deleteCourse" value="<?= $fetch_course['id'] ?>"><i class="fas fa-trash-alt"></i>
+                                        </button>
+                                    </form>
+
+                                </td>
+
+
+
+                            </tr>
+                    <?php
+                        }
+                    }
+                    ?>
                 </tbody>
             </table>
         </div>
