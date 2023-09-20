5bf344bf9ec870994c43421a85ca3b0c947e8050
huydev <lamnhathuy0393418721@gmail.com>
2023-09-21 01:30:53 +0700

    chưa fix lỗi

--- admin/core/codeProductView.php
+++ admin/core/codeProductView.php
@@ -16,7 +16,7 @@ if (isset($_POST['addProduct'])) {
     createNewProduct($name, $image, $price, $description, $category_id);
 }
 
-if (isset($_POST['updateProduct'])) {
+if (isset($_POST['editProduct'])) {
     $product_id =  mysqli_real_escape_string($conn, $_POST['product_id']);
     $name =  mysqli_real_escape_string($conn, $_POST['name']);
     $image =  mysqli_real_escape_string($conn, $_POST['image']);
@@ -24,28 +24,6 @@ if (isset($_POST['updateProduct'])) {
     $description =  mysqli_real_escape_string($conn, $_POST['description']);
     $category_id =  mysqli_real_escape_string($conn, $_POST['category_id']);
     editCurrentProduct($name, $image, $price, $description, $category_id, $product_id);
-}
-
-if (isset($_POST['editProduct'])) {
-    $product_id =  mysqli_real_escape_string($conn, $_POST['product_id']);
-    $name =  mysqli_real_escape_string($conn, $_POST['name']);
-    $price =  mysqli_real_escape_string($conn, $_POST['price']);
-    $description =  mysqli_real_escape_string($conn, $_POST['description']);
-    $category_id =  mysqli_real_escape_string($conn, $_POST['category_id']);
-
-    // Kiểm tra xem người dùng có tải lên hình ảnh mới không
-    if (!empty($_FILES['image']['name'])) {
-        $image = $_FILES['image']['name'];
-        $image_tmp = $_FILES['image']['tmp_name'];
-        $targetDirectory = "./admin/upload/"; // Thay đổi đường dẫn tùy theo thư mục lưu trữ của bạn
-        $targetPath = $targetDirectory . $image;
-        move_uploaded_file($image_tmp, $targetPath);
-    } else {
-        // Nếu không có hình ảnh mới, giữ nguyên hình ảnh cũ
-        $image = mysqli_real_escape_string($conn, $_POST['old_image']);
-    }
-
-    editCurrentProduct($product_id, $name, $image, $price, $description, $category_id);
 }
 
 
